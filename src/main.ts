7abe890a41d61114b42244666dbb7167ed701cb1
Wei Chun <tanweichun@outlook.com>
2022-01-02 13:42:25 +0000

    added config for deployment

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.use(cors(corsOptions));
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
